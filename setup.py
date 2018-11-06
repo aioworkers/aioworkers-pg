ef9733a534701ee312a867957383173bd0c08454
Alexander Bogushov <abogushov@gmail.com>
2018-11-06 11:55:55 +0500

    Return 3.5

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],

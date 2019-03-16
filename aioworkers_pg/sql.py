9ed07055af493a076b1380c5263ecfceeccc0e9b
Alexander Malev <malev@somedev.ru>
2019-03-16 22:47:42 +0300

    submodule sql

---
+++ aioworkers_pg/sql.py
@@ -0,0 +1,68 @@
+import re
+from collections import ChainMap
+from typing import Match, Any, Sequence
+
+NAME = re.compile(r'[^:]:([\d\w_]+)')
+
+
+class Table:
+    def __init__(self, name):
+        self.name = name
+
+    def select(self, *fields, **where):
+        sql = ['SELECT']
+        if fields:
+            sql.append(', '.join(fields))
+        else:
+            sql.append('*')
+        sql.extend(('FROM', self.name))
+        if where:
+            sql.append('WHERE')
+            sql.append(' AND '.join(' = '.join((k, ':' + k)) for k in where))
+        return SQL(' '.join(sql), **where)
+
+
+class SQL:
+    __slots__ = ('_sql', '_compiled', '_args', '_data')
+
+    def __init__(self, sql, *compiled, **kwargs):
+        self._sql = sql
+        self._compiled = compiled or self._get_params(sql)
+        self._args = self._get_args(kwargs) if kwargs else (self._compiled[0],)
+        self._data = kwargs
+
+    def with_data(self, *args, **kwargs):
+        m = ChainMap(kwargs, *reversed(args))
+        cls = type(self)
+        return cls(self._sql, *self._compiled, **m)
+
+    @staticmethod
+    def _get_params(sql: str) -> Sequence[Any]:
+        result = [sql]
+        pos = {}
+
+        def repl(m: Match) -> str:
+            name = m.group(1)
+            if name in pos:
+                position = pos[name]
+            else:
+                position = pos.setdefault(name, len(result))
+                result.append(name)
+            return m.group(0).replace(':' + name, '${}'.format(position))
+
+        result[0] = NAME.sub(repl, sql)
+        return tuple(result)
+
+    def _get_args(self, data):
+        args = [data.get(k) for i, k in enumerate(self._compiled)]
+        args[0] = self._compiled[0]
+        return tuple(args)
+
+    def __repr__(self):
+        return self._sql
+
+    def __iter__(self):
+        yield from self._args
+
+    def __len__(self):
+        return len(self._args)
---
+++ tests/test_sql.py
@@ -0,0 +1,58 @@
+import pytest
+
+from aioworkers_pg.sql import SQL, Table
+
+
+@pytest.fixture
+def config(dsn):
+    from aioworkers.core.config import Config
+    return Config(
+        db={
+            'cls': 'aioworkers_pg.base.Connector',
+            'dsn': dsn,
+        },
+    )
+
+
+async def test_select(context, recreate_table):
+    await recreate_table('x', 'a text')
+    sql = Table('x').select('a', a='q')
+    assert list(sql) == ['SELECT a FROM x WHERE a = $1', 'q']
+    await context.db.fetch(*sql)
+    sql = Table('x').select()
+    assert list(sql) == ['SELECT * FROM x']
+    await context.db.fetch(*sql)
+
+
+def test_sql1():
+    sql = SQL('SELECT :p')
+    assert sql._compiled == ('SELECT $1', 'p')
+    assert list(sql) == ['SELECT $1']
+
+    sql = sql.with_data(p=1)
+    assert sql._compiled == ('SELECT $1', 'p')
+    assert list(sql) == ['SELECT $1', 1]
+    assert repr(sql) == 'SELECT :p'
+
+    sql = sql.with_data({'p': 1})
+    assert sql._compiled == ('SELECT $1', 'p')
+    assert list(sql) == ['SELECT $1', 1]
+    assert repr(sql) == 'SELECT :p'
+
+    sql = sql.with_data({'p': 1}, p=2)
+    assert list(sql) == ['SELECT $1', 2]
+
+
+async def test_sql2(context):
+    sql = SQL('SELECT :p::int, :p', p=1)
+    assert sql._compiled == ('SELECT $1::int, $1', 'p')
+    assert list(sql) == ['SELECT $1::int, $1', 1]
+    await context.db.fetchrow(*sql)
+
+
+async def test_sql3(context, recreate_table):
+    await recreate_table('reports')
+    sql = SQL('SELECT * FROM reports', p=1)
+    assert sql._compiled == ('SELECT * FROM reports',)
+    assert list(sql) == ['SELECT * FROM reports']
+    await context.db.fetchrow(*sql)

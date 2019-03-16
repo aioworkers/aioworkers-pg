fed76d8f990d3499b315f775f3756c7a738d1897
Alexander Malev <malev@somedev.ru>
2019-03-16 22:46:42 +0300

    conftest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,11 @@
+import os
+
 import pytest
+
+
+@pytest.fixture(scope='session')
+def dsn():
+    return os.environ.get('PG_DSN', 'postgresql://localhost/test')
 
 
 @pytest.fixture
@@ -6,3 +13,11 @@ def context(config, loop):
     from aioworkers.core.context import Context
     with Context(config, loop=loop) as ctx:
         yield ctx
+
+
+@pytest.fixture
+def recreate_table(context):
+    async def recreate_table(name, *fields):
+        await context.db.execute('DROP TABLE IF EXISTS {}'.format(name))
+        await context.db.execute('CREATE TABLE {} ({})'.format(name, ','.join(fields)))
+    return recreate_table

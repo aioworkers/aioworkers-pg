20393728ff614c8caa64ca177256b9a442db92ac
Alexander Bogushov <abogushov@gmail.com>
2021-02-09 18:17:14 +0500

    #8: Black formatting.

--- aioworkers_pg/sa.py
+++ aioworkers_pg/sa.py
@@ -5,21 +5,24 @@ from .base import Connector as BaseConnector
 class Connector(BaseConnector):
     async def pool_factory(self, config):
         import asyncpgsa
+
         pool = await asyncpgsa.create_pool(
-            config.dsn, init=self._init_connection,
+            config.dsn,
+            init=self._init_connection,
         )
-        self.logger.debug('Create pool with address %s', config.dsn)
+        self.logger.debug("Create pool with address %s", config.dsn)
         return pool
 
     async def _init_connection(self, connection):
         import json
+
         # TODO: Need general solution to add codecs
         # https://github.com/aioworkers/aioworkers-pg/issues/1
         # Add codecs for json.
-        for t in ['json', 'jsonb']:
+        for t in ["json", "jsonb"]:
             await connection.set_type_codec(
                 t,
                 encoder=lambda x: x,
                 decoder=json.loads,
-                schema='pg_catalog',
+                schema="pg_catalog",
             )

dffa19be803ea68a58be6d7a77a33c5d42f070ec
Alexander Malev <aamalev@users.noreply.github.com>
2021-02-15 10:05:20 +0300

    #1: support custom connection.init (#13)

--- aioworkers_pg/base.py
+++ aioworkers_pg/base.py
@@ -1,15 +1,27 @@
+from typing import Awaitable, Callable, Optional
+
 import asyncpg
 from aioworkers.core.base import AbstractConnector
+from aioworkers.core.config import ValueExtractor
 
 
 class Connector(AbstractConnector):
+    _connection_init: Optional[Callable[[asyncpg.Connection], Awaitable]]
+
     def __init__(self, *args, **kwargs):
         self._pool = None
         super().__init__(*args, **kwargs)
 
-    def set_config(self, config):
-        cfg = config.new_parent(logger="aioworkers_pg")
+    def set_config(self, config: ValueExtractor) -> None:
+        cfg = config.new_parent(logger=__package__)
         super().set_config(cfg)
+        connection_init: Optional[str] = self.config.get(
+            "connection.init",
+        )
+        if connection_init:
+            self._connection_init = self.context.get_object(connection_init)
+        else:
+            self._connection_init = self._default_connection_init
 
     @property
     def pool(self) -> asyncpg.pool.Pool:
@@ -24,10 +36,10 @@ class Connector(AbstractConnector):
         if self._pool is None:
             self._pool = await self.pool_factory(self.config)
 
-    async def pool_factory(self, config):
+    async def pool_factory(self, config) -> asyncpg.pool.Pool:
         pool = await asyncpg.create_pool(
             config.dsn,
-            init=self._init_connection,
+            init=self._connection_init,
         )
         self.logger.debug("Create pool with address %s", config.dsn)
         return pool
@@ -38,7 +50,7 @@ class Connector(AbstractConnector):
             await self._pool.close()
             self._pool = None
 
-    async def _init_connection(self, connection):
+    async def _default_connection_init(self, connection: asyncpg.Connection):
         import json
 
         # TODO: Need general solution to add codecs

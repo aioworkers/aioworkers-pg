e0e6af4df60c5de41eea2156811992738e37d74a
Alexander Malev <malev@somedev.ru>
2023-07-16 01:51:36 +0300

    up test with loop

--- tests/test_pool_close.py
+++ tests/test_pool_close.py
@@ -2,14 +2,14 @@ from aioworkers.core.config import Config
 from aioworkers.core.context import Context
 
 
-async def test_pool_close(loop, dsn):
+async def test_pool_close(dsn):
     conf = Config(
         db={
             "cls": "aioworkers_pg.base.Connector",
             "dsn": dsn,
         },
     )
-    async with Context(conf, loop=loop) as c:
+    async with Context(conf) as c:
         assert c.db._pool is not None
         assert not c.db._pool._closed
     assert c.db._pool is None
